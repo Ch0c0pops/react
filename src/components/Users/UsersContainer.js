fc8aefefce1434aeceec869cb4a8aa1edc66d287
Stanislav Dolgopolov <ch0c0lateroollzz@yandex.ru>
2020-03-19 22:19:38 +0300

    react51

--- src/components/Users/UsersContainer.js
+++ src/components/Users/UsersContainer.js
@@ -5,15 +5,13 @@ import {followAC, setUsersAC, unfollowAC} from "../Redux/UsersReducer";
 
 
 const mapStateToProps =(state)=>{
-    return {
-        users: state.usersPage.users
-    }
+    return {users: state.usersPage.users}
 };
 const mapDispatchToProps=(dispatch)=>{
     return{
         follow: (userId)=> {dispatch(followAC(userId))},
         unfollow: (userId)=> {dispatch(unfollowAC(userId))},
-        setUsers: (users)=> {dispatch(setUsersAC(users))}
+        setUsers: (users)=> { dispatch(setUsersAC(users))}
     }
 };
 export default connect(mapStateToProps, mapDispatchToProps)(Users);
--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import React from "react";
 import {BrowserRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 
+const API_KEY = '5b5f72f1-a8a7-4748-9e84-4f7b48d7955a';
+
     ReactDOM.render(
         <BrowserRouter>
         <Provider store={store}>
