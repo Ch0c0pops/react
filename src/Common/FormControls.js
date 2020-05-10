eb6d88944935ee7d3c2bbec6b68f9b8fdc2410a5
Stanislav Dolgopolov <ch0c0lateroollzz@yandex.ru>
2020-05-10 20:37:51 +0300

    react77

--- src/Common/FormControls.js
+++ src/Common/FormControls.js
@@ -1,29 +1,23 @@
 import React from 'react';
 import styles from './FormControls.module.css';
 
+//рефакторинг с применением HOC самостоятельный, код как у Димыча в концe выпуска 77.
 
-export const Textarea = ({input, meta, ...props}) => {
-
-    const hasError = meta.touched && meta.error;
-    return (<div >
-        <div >
-            <textarea className={hasError ? styles.error : ''} {...input} {...props} />
-        </div>
-        <div className={styles.forms + '' + styles.error}>
-            {hasError && <span className={hasError ? styles.textError : ''}>{meta.error}</span>}
-        </div>
-    </div>)
+const FormWrapper = (Element) => {
+    let wrappedForm =({input, meta, ...props}) =>{
+        const hasError = meta.touched && meta.error;
+        return(
+            (<div >
+                <div >
+                    <Element className={hasError ? styles.error : ''} {...input} {...props} />
+                </div>
+                <div className={styles.forms + '' + styles.error}>
+                    {hasError && <span className={hasError ? styles.textError : ''}>{meta.error}</span>}
+                </div>
+            </div>)
+        )
+    }
+    return wrappedForm;
 };
-
-export const Input = ({input, meta, ...props}) => {
-
-    const hasError = meta.touched && meta.error;
-    return (<div >
-        <div >
-            <input className={hasError ? styles.error : ''} {...input} {...props} />
-        </div>
-        <div className={styles.forms + '' + styles.error}>
-            {hasError && <span className={hasError ? styles.textError : ''}>{meta.error}</span>}
-        </div>
-    </div>)
-};
+export const Input =  FormWrapper('input');
+export const Textarea =  FormWrapper('textarea');
