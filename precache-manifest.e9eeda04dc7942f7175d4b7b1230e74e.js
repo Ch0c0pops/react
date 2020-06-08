d6d828a0d79142b2afd0bdfcd56d32c54024099c
Ch0c0pops <58004283+Ch0c0pops@users.noreply.github.com>
2020-06-08 23:02:19 +0300

    Updates

---
+++ precache-manifest.e9eeda04dc7942f7175d4b7b1230e74e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0785acd2e9d67d3f97563330239b914a",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "eb92f91a2b389d2fb13c",
+    "url": "/react/static/css/3.ed83894b.chunk.css"
+  },
+  {
+    "revision": "8f35ab76beb39c9d8f3d",
+    "url": "/react/static/css/4.2d47a688.chunk.css"
+  },
+  {
+    "revision": "e4ca4689964bb3413761",
+    "url": "/react/static/css/main.604cac11.chunk.css"
+  },
+  {
+    "revision": "b5d9aaf5b6af3fd484ce",
+    "url": "/react/static/js/2.26c283f0.chunk.js"
+  },
+  {
+    "revision": "eb92f91a2b389d2fb13c",
+    "url": "/react/static/js/3.07ebd8b0.chunk.js"
+  },
+  {
+    "revision": "8f35ab76beb39c9d8f3d",
+    "url": "/react/static/js/4.aa3fd876.chunk.js"
+  },
+  {
+    "revision": "e4ca4689964bb3413761",
+    "url": "/react/static/js/main.fdaebb62.chunk.js"
+  },
+  {
+    "revision": "91481622955a2d344354",
+    "url": "/react/static/js/runtime-main.3ca0b961.js"
+  },
+  {
+    "revision": "0d0345f662a757ef17eef497fa8e83fc",
+    "url": "/react/static/media/preLoader.0d0345f6.gif"
+  },
+  {
+    "revision": "032a58a0a36713f5b92bcd97a33c3301",
+    "url": "/react/static/media/userAvatar.032a58a0.jpg"
+  }
+]);
